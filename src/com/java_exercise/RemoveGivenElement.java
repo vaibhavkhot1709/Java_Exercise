4145822c725f046da9f33d38d0ded480c0cef4e9
vaibhavkhot1709 <vaibhavkhot1617gmail.com>
2022-09-18 16:42:49 +0530

    second commit

--- src/com/java_exercise/RemoveGivenElement.java
+++ src/com/java_exercise/RemoveGivenElement.java
@@ -3,23 +3,27 @@ package com.java_exercise;
 import java.util.Arrays;
 
 public class RemoveGivenElement {
-	public static void removeGivenElement() {
+	public static int [] removeGivenElement() {
 		int num[] = { 10, 30, 20, 70, 55 };
 		int count=0;
-		int j=0,pos=2;
+		int j=0,pos=22;
 
 		for (int i = 0; i < num.length; i++) {
-			if (num[i] == num[pos]) 
+			if (num[i] ==pos) 
 				count++;
 		}
 		
 		int num2[] = new int[num.length-count];
 		
-		for (int i = 0; i < num.length; i++) {
-			if (num[i] !=num[pos]) 
+		for (int i = 0; i < num.length-1; i++) {
+			if (num[i] !=pos) 
 				num2[j]=num[i];
 			j++;
 		}
+		for(int n:num2) {
+			System.out.println(n);
+		}
+		return num2;
 		
 	}
 
